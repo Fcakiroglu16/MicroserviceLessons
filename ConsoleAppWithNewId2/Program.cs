90f7fe7cd1e303cec42a63fa84b3f55dd51fdedd
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2022-08-30 18:25:40 +0300

    GUID completed

---
+++ ConsoleAppWithNewId2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MassTransit;
+
+Console.WriteLine("Hello, World!");
+
+foreach (var i in Enumerable.Range(1,20).ToList())
+{
+    Thread.Sleep(1000);
+    Console.WriteLine(NewId.Next());
+}
