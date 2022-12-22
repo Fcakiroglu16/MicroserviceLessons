50fcb798439cc9f9a85852f47eac155ab825c22b
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2022-12-23 02:38:33 +0300

    clear

--- RSACreate.App/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Security.Cryptography;
-
-using var rsa = RSA.Create();
-Console.WriteLine(
-    $"-----Private key-----{Environment.NewLine}{Convert.ToBase64String(rsa.ExportRSAPrivateKey())}{Environment.NewLine}");
-Console.WriteLine($"-----Public key-----{Environment.NewLine}{Convert.ToBase64String(rsa.ExportRSAPublicKey())}");
